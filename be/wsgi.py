2162ccd2e81dde2f1358ea1a85c4aae0133a23e2
Sayali Pathare <60076505+sayalipathare@users.noreply.github.com>
2021-03-30 20:30:22 -0400

    Delete be directory

--- be/wsgi.py
+++
@@ -1,8 +0,0 @@
-# The wsgi.py file creates an application object (or callable) for the Gunicorn server
-# so that the server can use it. Each time a request comes, the server uses this application 
-# object to run the application’s request handlers upon parsing the URL.
-
-from twtr import app
-
-if __name__ == "__main__":
-  app.run()
